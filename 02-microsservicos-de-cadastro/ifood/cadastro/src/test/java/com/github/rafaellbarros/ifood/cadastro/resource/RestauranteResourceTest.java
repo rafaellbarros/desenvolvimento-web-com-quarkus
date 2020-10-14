991c70f0aacd2345c8b15cebd0f70113c8640a32
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-14 03:46:39 -0300

    :construction: Testando endpoints protegidos

--- 02-microsservicos-de-cadastro/ifood/cadastro/src/test/java/com/github/rafaellbarros/ifood/cadastro/resource/RestauranteResourceTest.java
+++ 02-microsservicos-de-cadastro/ifood/cadastro/src/test/java/com/github/rafaellbarros/ifood/cadastro/resource/RestauranteResourceTest.java
@@ -5,14 +5,18 @@ import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.configuration.Orthography;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.rafaellbarros.ifood.cadastro.config.CadastroTestLifeCycleManager;
+import com.github.rafaellbarros.ifood.cadastro.model.dto.AtualizarRestauranteDTO;
 import com.github.rafaellbarros.ifood.cadastro.model.entity.Restaurante;
+import com.github.rafaellbarros.ifood.cadastro.util.TokenUtils;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.http.Header;
 import io.restassured.specification.RequestSpecification;
 import org.approvaltests.Approvals;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.Response;
@@ -25,6 +29,13 @@ import static io.restassured.RestAssured.given;
 @QuarkusTestResource(CadastroTestLifeCycleManager.class)
 public class RestauranteResourceTest {
 
+    private String token;
+
+    @BeforeEach
+    public void  gereToken() throws Exception {
+        token = TokenUtils.generateTokenString("/JWTProprietarioClaims.json", null);
+    }
+
     @Test
     @DataSet("restaurantes-cenario-1.yml")
     public void testBuscarRestaurantes() {
@@ -36,15 +47,16 @@ public class RestauranteResourceTest {
         Approvals.verifyJson(resultado);
     }
 
+
     @Test
     @DataSet("restaurantes-cenario-1.yml")
     public void testAlterarRestaurante() {
-        Restaurante restaurante = new Restaurante();
-        restaurante.nome= "novoNome";
+        AtualizarRestauranteDTO dto = new AtualizarRestauranteDTO();
+        dto.nomeFantasia = "novoNome";
         Long parameterValue = 123L;
         given()
                 .with().pathParam("id", parameterValue)
-                .body(restaurante)
+                .body(dto)
                 .when().put("/restaurantes/{id}")
                 .then()
                 .statusCode(Response.Status.NO_CONTENT.getStatusCode())
@@ -53,11 +65,11 @@ public class RestauranteResourceTest {
         Restaurante findById = Restaurante.findById(parameterValue);
 
         //poderia testar todos os outros atribudos
-        Assert.assertEquals(restaurante.nome, findById.nome);
+        Assert.assertEquals(dto.nomeFantasia, findById.nome);
 
     }
 
     private RequestSpecification given() {
-        return RestAssured.given().contentType(ContentType.JSON);
+        return RestAssured.given().contentType(ContentType.JSON).header(new Header("Authorization", "Bearer "+ token));
     }
 }
