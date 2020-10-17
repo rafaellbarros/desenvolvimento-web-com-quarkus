994f3021df731f51b8c42cae926b9e94c4773cc3
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-17 20:06:42 -0300

    :construction: Enviando Pedidos pro kafka

--- 04-microsservicos-de-pedido/ifood/marketplace/src/main/java/com/github/rafaellbarros/ifood/mp/model/entity/Prato.java
+++ 04-microsservicos-de-pedido/ifood/marketplace/src/main/java/com/github/rafaellbarros/ifood/mp/model/entity/Prato.java
@@ -35,6 +35,12 @@ public class Prato {
         return unitToMulti(preparedQuery);
     }
 
+    public static Uni<PratoDTO> findById(PgPool client, Long id) {
+        return client.preparedQuery("SELECT * FROM prato WHERE id = $1").execute(Tuple.of(id))
+                .map(RowSet::iterator)
+                .map(iterator -> iterator.hasNext() ? PratoDTO.from(iterator.next()) : null);
+    }
+
     private static Multi<PratoDTO> unitToMulti(Uni<RowSet<Row>> queryResult) {
         return queryResult.onItem()
                 .produceMulti(set -> Multi.createFrom().items(() -> {
