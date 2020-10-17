7cead7600aa4bc0850738230bda6ae074638bcf2
Rafael Barros <rafaelbarros.df@gmail.com>
2020-10-17 17:46:24 -0300

    :truck: Movendo projeto

---
+++ 04-microsservicos-de-pedido/ifood/cadastro/src/main/java/com/github/rafaellbarros/ifood/cadastro/resource/RestauranteResource.java
@@ -0,0 +1,177 @@
+package com.github.rafaellbarros.ifood.cadastro.resource;
+
+import com.github.rafaellbarros.ifood.cadastro.infra.ConstraintViolationResponse;
+import com.github.rafaellbarros.ifood.cadastro.model.dto.*;
+import com.github.rafaellbarros.ifood.cadastro.model.entity.Prato;
+import com.github.rafaellbarros.ifood.cadastro.model.entity.Restaurante;
+import com.github.rafaellbarros.ifood.cadastro.model.mapper.PratoMapper;
+import com.github.rafaellbarros.ifood.cadastro.model.mapper.RestauranteMapper;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Gauge;
+import org.eclipse.microprofile.metrics.annotation.SimplyTimed;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.json.bind.Jsonb;
+import javax.json.bind.JsonbBuilder;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Path("/restaurantes")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "restaurante")
+@RolesAllowed("proprietario")
+@SecurityScheme(securitySchemeName = "ifood-oauth", type = SecuritySchemeType.OAUTH2, flows = @OAuthFlows(password = @OAuthFlow(tokenUrl = "http://localhost:8180/auth/realms/ifood/protocol/openid-connect/token")))
+public class RestauranteResource {
+
+
+    @Inject
+    RestauranteMapper restauranteMapper;
+
+    @Inject
+    PratoMapper pratoMapper;
+
+    @Inject
+    @Channel("restaurantes")
+    Emitter<String> emitter;
+
+    @GET
+    @Counted(name = "Quantidade buscas Restaurante")
+    @SimplyTimed(name = "Tempo simples de busca")
+    @Timed(name = "Tempo completo de busca")
+    public List<RestauranteDTO> listarTodos() {
+        // TODO: Refactoring
+        Stream<Restaurante> restaurantes = Restaurante.streamAll();
+        return restaurantes.map(r -> restauranteMapper.toRestauranteDTO(r)).collect(Collectors.toList());
+    }
+
+    @POST
+    @Transactional
+    @APIResponse(
+	responseCode = "201",
+	description = "Caso restaurante seja cadastrado com sucesso")
+    @APIResponse(
+	responseCode = "400",
+	content = @Content(schema = @Schema(allOf = ConstraintViolationResponse.class))
+    )
+    public Response adicionar(@Valid AdicionarRestauranteDTO dto) {
+        final Restaurante restaurante = restauranteMapper.toRestaurante(dto);
+        restaurante.persist();
+
+        Jsonb create = JsonbBuilder.create();
+        final String json = create.toJson(restaurante);
+        emitter.send(json);
+
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public void atualizar(@PathParam("id") Long id, AtualizarRestauranteDTO dto) {
+        Optional<Restaurante> restauranteOptional = Restaurante.findByIdOptional(id);
+        if (restauranteOptional.isEmpty()) {
+            throw new NotFoundException();
+        }
+        Restaurante restarante = restauranteOptional.get();
+        restauranteMapper.toRestaurante(dto, restarante);
+        restarante.persist();
+    }
+
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public void deletar(@PathParam("id") Long id) {
+        Optional<Restaurante> produtoOptional = Restaurante.findByIdOptional(id);
+        produtoOptional.ifPresentOrElse(Restaurante::delete, () -> {
+            throw new NotFoundException();
+        });
+    }
+
+    // Pratos
+    @GET
+    @Path("{idRestaurante}/pratos")
+    @Tag(name = "prato")
+    public List<PratoDTO> buscarPratos(@PathParam("idRestaurante") Long idRestaurante) {
+        Optional<Restaurante> restauranteOptional = Restaurante.findByIdOptional(idRestaurante);
+        if (restauranteOptional.isEmpty()) {
+            throw new NotFoundException("Restaurante não existe");
+        }
+        Stream<Prato> pratos = Prato.stream("restaurante", restauranteOptional.get());
+        return pratos.map(p -> pratoMapper.toDTO(p)).collect(Collectors.toList());
+    }
+
+    @POST
+    @Tag(name = "prato")
+    @Path("{idRestaurante}/pratos")
+    @Transactional
+    @Tag(name = "prato")
+    public Response adicionarPrato(@PathParam("idRestaurante") Long idRestaurante, AdicionarPratoDTO dto) {
+        Optional<Restaurante> restauranteOptional = Restaurante.findByIdOptional(idRestaurante);
+        if (restauranteOptional.isEmpty()) {
+            throw new NotFoundException("Restaurante não existe");
+        }
+
+        Prato prato = pratoMapper.toPrato(dto);
+        prato.restaurante = restauranteOptional.get();
+        prato.persist();
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("{idRestaurante}/pratos/{id}")
+    @Transactional
+    @Tag(name = "prato")
+    public void atualizarPrato(@PathParam("idRestaurante") Long idRestaurante, @PathParam("id") Long id, AtualizarPratoDTO dto) {
+        Optional<Restaurante> restauranteOptional = Restaurante.findByIdOptional(idRestaurante);
+        if (restauranteOptional.isEmpty()) {
+            throw new NotFoundException("Restaurante não existe");
+        }
+
+        //No nosso caso, id do prato vai ser único, independente do restaurante...
+        Optional<Prato> pratoOptional = Prato.findByIdOptional(id);
+        if (pratoOptional.isEmpty()) {
+            throw new NotFoundException("Prato não existe");
+        }
+
+        Prato prato = pratoOptional.get();
+        pratoMapper.toPrato(dto, prato);
+        prato.persist();
+    }
+
+    @DELETE
+    @Path("{idRestaurante}/pratos/{id}")
+    @Transactional
+    @Tag(name = "prato")
+    public void deletarPrato(@PathParam("idRestaurante") Long idRestaurante, @PathParam("id") Long id) {
+        Optional<Restaurante> restauranteOptional = Restaurante.findByIdOptional(idRestaurante);
+        if (restauranteOptional.isEmpty()) {
+            throw new NotFoundException("Restaurante não existe");
+        }
+
+        Optional<Prato> pratoOptional = Prato.findByIdOptional(id);
+        pratoOptional.ifPresentOrElse(Prato::delete, () -> {
+            throw new NotFoundException();
+        });
+    }
+}
---
+++ 04-microsservicos-de-pedido/ifood/marketplace/src/main/java/com/github/rafaellbarros/ifood/mp/model/entity/PratoCarrinho.java
@@ -0,0 +1,9 @@
+package com.github.rafaellbarros.ifood.mp.model.entity;
+
+public class PratoCarrinho {
+
+    public String usuario;
+
+    public Long prato;
+
+}
