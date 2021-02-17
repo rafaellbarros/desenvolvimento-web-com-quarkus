14c8bdb26901dff858a289ff7215f56eda7262ef
Rafael Barros <rafaelbarros.df@gmail.com>
2021-02-17 16:23:22 -0300

    :construction: Log centralizado

--- 05-diversos/ifood/pedido/src/main/java/com/github/rafaellbarros/ifood/pedido/resource/HelloResource.java
+++ 05-diversos/ifood/pedido/src/main/java/com/github/rafaellbarros/ifood/pedido/resource/HelloResource.java
@@ -1,16 +1,23 @@
 package com.github.rafaellbarros.ifood.pedido.resource;
 
+import org.jboss.logging.Logger;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.time.LocalDateTime;
 
 @Path("/hello")
 public class HelloResource {
 
+    private static final Logger LOG = Logger.getLogger(HelloResource.class);
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        LOG.info("*************** start ***************\n");
+        LOG.infov("Olá Quarkus {0} ", LocalDateTime.now());
         return "hello";
     }
 }
